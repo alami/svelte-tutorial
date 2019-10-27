8fd0400ed11c441ac300e6d920299574d9f3d823
Aminov <alami@tshtt.uz>
2019-10-27 14:48:39 +0500

    new article 446026

---
+++ src/A4446026.svelte
@@ -0,0 +1,13 @@
+<ul>
+    {#each profiles as profile}
+        <li>{profile.name}: {profile.role}</li>
+    {/each}
+</ul>
+
+<script>
+    const profiles = [
+        {name: 'Wes Bos', role: 'React extraordinaire'},
+        {name: 'Chris Coyier', role: 'Father of CodePen'},
+        {name: 'Cassidy Williams', role: 'Letting you know it\'s pi time'}
+    ];
+</script>
--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,17 @@
 <script>
+	import A446026 from "./A4446026.svelte";
 	import Fetch from "./Fetch.svelte";
 	import Form from "./Form.svelte";
+	import A4446026 from "./A4446026.svelte";
+
 	let searchTerm;
+
 	function handleSubmit() {
-		const { value } = this.elements.search;
+		const {value} = this.elements.search;
 		searchTerm = value;
 	}
 </script>
+<A4446026 /><hr/><hr/>
 <Form {handleSubmit} />
 <Fetch {searchTerm} let:data>
 	<h1>A list of links</h1>
